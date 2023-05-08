944a57a340da2a10e83e9a291d99f750c8acd360
kohei0429 <taratyaan@gmail.com>
2023-05-08 23:02:57 +0900

    factorybot修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,12 +3,7 @@ FactoryBot.define do
     name                  {'test'}
     email                 {'test@example'}
     password              {'000000'}
-<<<<<<< Updated upstream
-    password_confirmation {password}
-    profire               {xxxxxxxx}
-=======
     password_confirmation {'password'}
     profire               {'abc'}
->>>>>>> Stashed changes
   end
 end
