b7fe53782721f9ba31511913459490305f8ed54a
kohei0429 <taratyaan@gmail.com>
2023-05-11 07:13:21 +0900

    環境変数を読み込む記述に変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 #全てのコントローラーは、ApplicationControllerの処理を受け継ぐ
   before_action :basic_auth
    #Basic認証呼び出し
-   
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   #before_action, メソッドの各アクションの事前処理(優先)呼び出し
   #if, で処理対象をdevise_controllerのアクションに限定
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'shibao' && password == '0429'
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
     end
   end
   #Basic認証
