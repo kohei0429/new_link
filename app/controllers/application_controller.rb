2db038b82561554d32ab0916a219d7028a6cb03b
kohei0429 <taratyaan@gmail.com>
2023-05-11 07:03:00 +0900

    Basic認証導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
 #全てのコントローラーは、ApplicationControllerの処理を受け継ぐ
-
+  before_action :basic_auth
+   #Basic認証呼び出し
+   
   before_action :configure_permitted_parameters, if: :devise_controller?
   #before_action, メソッドの各アクションの事前処理(優先)呼び出し
   #if, で処理対象をdevise_controllerのアクションに限定
@@ -8,6 +10,13 @@ class ApplicationController < ActionController::Base
 
 
   private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'shibao' && password == '0429'
+    end
+  end
+  #Basic認証
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profire])
