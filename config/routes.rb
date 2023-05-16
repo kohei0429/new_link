32cfc06aebc78ba585095e75f56466b53c8183a4
kohei0429 <taratyaan@gmail.com>
2023-05-16 20:51:29 +0900

    tweetsコントローラー、indexアクション設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'top/index'
   devise_for :users
   root 'top#index'
+  resources :tweets, only: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
