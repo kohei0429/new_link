7957a3b8cf4c5e2374d093c72022971dc3f2aecf
kohei0429 <taratyaan@gmail.com>
2023-05-20 14:38:41 +0900

    newアクションのルーティングを追記

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,4 +4,8 @@ class TweetsController < ApplicationController
     # allメソッドを使用して、tweetsテーブルすべてのレコードをインスタンス変数に代入し、ビューに受け渡す
     # この@tweetsは、ビューファイルで使用できる
   end
+
+  def new
+    @tweet = Tweet.new
+  end
 end
