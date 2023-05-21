081ad849736537d782d623fed9312b13b21249d3
kohei0429 <taratyaan@gmail.com>
2023-05-21 20:04:21 +0900

    destoroyアクション定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,6 +13,11 @@ class TweetsController < ApplicationController
     Tweet.create(tweet_params)
   end
 
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+  
   private
   def tweet_params
     params.require(:tweet).permit(name, :image, :text)
