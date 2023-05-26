90b82b101a0dd2f6ac43155cbdca0246e595519c
kohei0429 <taratyaan@gmail.com>
2023-05-26 21:16:31 +0900

    記述順番変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show' # マイページ
-  get 'tweets/index'
   devise_for :users
   root 'tweets#index'
+  resources :tweets, only: [:index, :new, :create, :destroy, :show]
+  get 'users/show' # マイページ
+  get 'tweets/index'
   resources :tweets, only: [:index, :new, :create, :destroy, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
