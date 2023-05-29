55b74cb288d4a450ce05a035dbe6fa71edab18bf
kohei0429 <taratyaan@gmail.com>
2023-05-29 09:45:17 +0900

    無駄な記述を削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   root 'tweets#index'
   resources :tweets, only: [:index, :new, :create, :destroy, :show]
   get 'users/show' # マイページ
-  resources :tweets, only: [:index, :new, :create, :destroy, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
