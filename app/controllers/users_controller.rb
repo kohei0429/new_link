b2615483188e42bb3a42acc46cedd10dbc61142d
kohei0429 <taratyaan@gmail.com>
2023-05-30 09:51:56 +0900

    変数userを使用する為、current_userを変数userに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @name = user.name
-    @tweets = current_user.tweets
+    @tweets = user.tweets
   end
 
   private
