fd6d766875c2db012cd40d4b5775ae89f9cb90dd
kohei0429 <taratyaan@gmail.com>
2023-06-11 12:32:22 +0900

    コメントアウト削除

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,4 @@ Rails.application.routes.draw do
     resources :comments, only: :create
   end
   get 'users/show' # マイページ
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
