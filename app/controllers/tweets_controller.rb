9f88dff514720a156cb514f48cdf71e06b3ab135
kohei0429 <taratyaan@gmail.com>
2023-06-17 10:08:57 +0900

    activerecord導入後、エラーが出るのでimageカラムを削除

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -28,7 +28,7 @@ class TweetsController < ApplicationController
   private
 
   def tweet_params
-    params.require(:tweet).permit(:image, :text).merge(user_id: current_user.id)
+    params.require(:tweet).permit(:text).merge(user_id: current_user.id)
   end
   
   def move_to_index
